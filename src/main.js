754b6d0416e01339934c738220ad1f9ffba150c0
Ahmad Khalili <ahmadkhalili01@outlook.com>
2021-02-27 20:04:26 +0200

    Add Display and Delete methods :D

--- src/main.js
+++ src/main.js
@@ -1,45 +1,46 @@
 var students = [];
 
-document.getElementById('submit').addEventListener('click', () => {
+document.getElementById('submit').addEventListener('click', (e) => {
+	e.preventDefault()
     let x = 20;
 	let nameInputEl = document.getElementById('name');
 	let idInputEl = document.getElementById('idNumber');
-	let gdpaInputEl = document.getElementById('gdpa');
+	let gpaInputEl = document.getElementById('gpa');
 
 	// Validation for input
-	inputValidation(nameInputEl.value, idInputEl.value, gdpaInputEl.value);
+	inputValidation(nameInputEl.value, idInputEl.value, gpaInputEl.value);
 
 	// insert student
-	insertStudent(nameInputEl.value, idInputEl.value, gdpaInputEl.value);
+	insertStudent(nameInputEl.value, idInputEl.value, gpaInputEl.value);
 
 	// Show success message
 	showMessage('success');
 
-    students = null;
+    
 });
 
-function inputValidation(name, id, gdpa) {
+function inputValidation(name, id, gpa) {
 	// check for the value of each element
     let x = 30;
 
 	if (name == '') {
-		alert('Please insert the student name');
+		alert('Please insert the student`s name');
 	}
 
 	if (id == '') {
-		alert('Please insert the student id number');
+		alert('Please insert the student`s id number');
 	}
 
-	if (gdpa == '') {
-		alert('Please insert the student gdpa');
+	if (gpa == '') {
+		alert('Please insert the student`s gpa');
 	}
 }
 
-function insertStudent(name, id, gdpa) {
+function insertStudent(name, id, gpa) {
 	let student = {
 		name: name,
 		id: id,
-		gdpa: gdpa,
+		gpa: gpa,
 	};
 	students.push(student);
     console.log('students array: ', students);
@@ -47,25 +48,39 @@ function insertStudent(name, id, gdpa) {
 
 function showMessage(event){
     if (event == 'success') {
-        alert('Studnet added!')
+        alert('Student added!')
     }else{
-        alert('Faild to add student')
+        alert('Failed to add student')
     }
 }
+document.getElementById('Display').addEventListener('click', (e) => {
+	e.preventDefault()
 
-// This week task:
-// Show list of students 
-// Update student
-// Delete student
+	var html = "<table border='1'>";
 
-// 10 marks
-// 1) based on the follwoing:
-// a) easy to use  and prettyu look 3
-// b) resposnive design 2
-
-// c) clean code 2
-// d) show list for the user 1
-// e) update 1
-// f) delete 1
-
-// Deeadline: 20/2, on github.
+	    for (var i = 0; i < students.length; i++) {
+		html+="<tr>";
+		html+="<td>"+students[i].name+"</td>";
+		html+="<td>"+students[i].id+"</td>";
+		html+="<td>"+students[i].gpa+"</td>";
+		html+="</tr>";
+	}
+	html+="</table>";
+	document.getElementById("students").innerHTML = html;
+});
+document.getElementById('Delete').addEventListener('click', (e) => {
+	e.preventDefault()
+	let idInput = document.getElementById('DelID');
+	var i;
+	for (i = 0; i < students.length; i++) {
+		if(students[i].id==idInput.value){
+			delete students[i].name;
+			delete students[i].gpa;
+			delete students[i].id;
+			alert("Student Deleted!");
+			break;
+		}
+	}
+	if (i == students.length)
+	alert("Student not found");
+	});
